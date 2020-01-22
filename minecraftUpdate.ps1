fe7861da3b5c83d3c1aa8023fe86d78f06e489db
Nathan Ferguson <nathan@home.lab>
2020-01-22 09:31:12 -0800

    Modified script, added backups, server start

--- minecraftUpdate.ps1
+++ minecraftUpdate.ps1
@@ -1,11 +1,9 @@
-# # # # # # # # # # # # # # # # # # # # #
-# # # # # # # CONFIGURATION # # # # # # #
-# # # # # # # # # # # # # # # # # # # # #
 $saveJarLocation = "e:\minecraftdata\"
 $updateStartScript = $TRUE
 $startServerFile = "e:\minecraftdata\ServerStart.bat"
-# # # # # # # # # # # # # # # # # # # # #
-
+$mcBackup = "e:\mcBackup"
+$today = Get-Date -Format MMddyyyymm
+$networkBackupPath = '\\virthost1\vault\network-backups\minecraftbackups'
 
 # Stop Minecraft server
 Write-Host "Checking for running minecraft server"
@@ -18,81 +16,86 @@ if ($null -ne $proc) {
     Write-Host "No running minecraft server found, starting server backup "
 }
 
+$backupFolderExists = (Test-Path $mcBackup)
 
+if ($backupFolderExists -ne $true) {
+    Write-Output "missing backup folder, creating..."
+    New-Item -Path 'e:\mcBackup' -ItemType 'directory'
+}else {
+    Write-Output "Backup folder exists"
+}
+
+$destination = "$mcbackup\mcBackup$today.zip"
+$target = $mcData
+
+$backupResult = (7z a -tzip $destination $target -r -xr!System*)
+
+if (($backupResult | findstr "Everything is Ok") -ne '') {
+    Write-Output "backup completed successfully"
+}
+
+Write-Host "Getting verision manifest"
 $jsonVersions = Invoke-WebRequest -Uri https://launchermeta.mojang.com/mc/game/version_manifest.json | ConvertFrom-Json
+$latestVersion = $jsonVersions.latest.release
+Write-Host "Latest version is $latestVersion"
 
-
-# Get all available versions so we can find the latest's data
-$minecraftVersions = $jsonVersions.versions
-
-# Loop through each version until we find the latest version
-#for ($i=0; $i -lt $minecraftVersions.Count; $i++)
-#{
-    # Is this found version id the latest version?
-#    if ($minecraftVersions[$i].id -eq $minecraftLatestVersion)
-#    {
-        # We found the right version, get its JSON url
-#        $jsonUrlLatestVersion = $minecraftVersions[$i].url
-        
-        # Break out of the loop since we have what we need
-#        break
-#    }
-#    else {
-       #blah
-#    }
-#}
-
- Write-Output "taking the first in the aray"
-$minecraftLatestVersion = $minecraftVersions[0].ID
-$jsonUrlLatestVersion = $minecraftVersions[0].url
-# Get Latest Version Manifest and convert to PowerShell JSON Object
-$jsonLatestVersion = Invoke-WebRequest -Uri $jsonUrlLatestVersion | ConvertFrom-Json
+$downloadVersion = $jsonVersions.versions | Where-Object id -eq $latestVersion
+$jsonLatestVersion = Invoke-WebRequest -Uri $downloadVersion.url | ConvertFrom-Json
 
 # Get the Server Jar URL
 $jarUrl = $jsonLatestVersion.downloads.server.url
 
 # Determine what the jar file name will be
 $minecraftJar = "minecraft_server." + $minecraftLatestVersion + ".jar"
+Write-Host "We'll name the server.jar $minecraftJar"
 
 # Set the location and file name to save the downloaded file to
 $jarPath = $saveJarLocation + $minecraftJar
+Write-Host "We'll write the new file to $jarPath"
 
-# Create a web client
-$webclient = New-Object System.Net.WebClient
+# check if it already exists
+if ($true -eq (Test-Path $jarPath)) {
+    Write-Host "That version already latest, nothing to do"
+}Elseif ($false -eq (Test-Path $jarPath)) {
+    Write-Host "Versions don't match, downloading latest"
+    # Create a web client
+    $webclient = New-Object System.Net.WebClient
 
-# Using the webclient, download the file in the $url to $jarPath
-$webclient.DownloadFile($jarUrl, $jarPath)
+    # Using the webclient, download the file in the $url to $jarPath
+    Write-Verbose
+    $webclient.DownloadFile($jarUrl, $jarPath)
 
-
-# If configuration value of $updateStartScript is True, update the Server Start script with new jar file
-if ($updateStartScript)
-{
-    # Get the contents of the Server Start script
-    $startServerContents = [string]::join("`n", (Get-Content $startServerFile))
-
-    # Set the pattern we want to find in the Server Start script so we can determine the version number
-    $pattern = "minecraft_server.(.*?).jar"
-
-    # Find the pattern in the Server Start script
-    $oldminecraftLatestVersion = [regex]::match($startServerContents, $pattern).Groups[1].Value
-
-    # Replace the found version number with the new version number
-    $startServerContents = $startServerContents -replace $oldminecraftLatestVersion, $minecraftLatestVersion
-
-    # Write the new start script to the Server Start script file
-    $startServerContents | Set-Content $startServerFile
 }
-else
-{
-    # Do not update the Server Start script
-}
+
+# Start the server
 $javaCommand = Get-Command java.exe
 $javaPath = $javaCommand.Name
 
 # Starting minecraft server
-e:
-cd \minecraftdata
+Write-Host "Starting Minecraft Server"
 Start-Process $javaPath -ArgumentList "-Xms3072m", "-Xmx4192m", "-d64", "-jar", $jarPath
 
-Write-Output "Sleeping 30 seconds"
-start-sleep -Seconds 30
+
+# Cleaning up backups
+if ((Test-Path $networkBackupPath) -ne $true) {
+    Write-Output "No network backup folder, creating..."
+    }else {
+    Write-Output "Network backup folder exists"
+}
+$filesToRemove = (Get-ChildItem -Path $mcBackup -Exclude *.ps1) | Where-Object LastWriteTime -lt (get-date).addDays(-3)
+foreach ($file in $filesToRemove) {
+    Write-Output "Removing local file $file"
+    Remove-Item $file
+}
+
+$networkFilesToRemove = (Get-ChildItem -Path $networkBackupPath -Recurse -Exclude *.ps1) | Where-Object LastWriteTime -lt (get-date).addDays(-14)
+foreach ($file in $networkFilesToRemove) {
+    Write-Output "Removing network file $file"
+    Remove-Item $file
+}
+
+Write-Output "Copying remaining files to $networkBackupPath"
+Copy-Item -Path $mcBackup -Destination $networkBackupPath -Recurse -Force
+
+Write-Host "Leaving this window open for 30 seconds"
+Start-Sleep -Seconds 30
